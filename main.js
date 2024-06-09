4ddc403d002809ed0d36a5146cf64c1fcf67fdba
Merced Ayala <mercy.ayala08@gmail.com>
2024-06-09 07:52:49 -0500

    Agregar métodos http y params

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Hola mundo con Node JS')
+
+// forma antigua de llamar libreria
+// const express = require('express')
+
+// forma actual con ECMAScript 6 de llamar librerias
+import express from 'express'
+
+
+const app = express()
+const port = 3000
